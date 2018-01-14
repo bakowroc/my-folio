aeb305e72880af007040cf91dee71676cedd0e28
bakowroc <kontakt@maciejbakowicz.pl>
2018-01-14 15:41:11 +0100

    Fixes

--- src/app/Project/Projects.data.ts
+++ src/app/Project/Projects.data.ts
@@ -3,244 +3,82 @@ import { ProjectInterface } from './Project.interface';
 export const ProjectsData: Array<ProjectInterface> = [
   {
     area: 'p1',
-    date: new Date(),
+    date: new Date("06/01/2017"),
     id: 1,
-    image: 'https://unsplash.it/1920/1080?random',
+    image: 'http://maciejbakowicz.pl/img/galeria/sampph.png',
     link: '#',
     technologies: [
       {
-        name: 'Angular',
+        name: 'AngularJS',
         percent: 60
       },
       {
-        name: 'Django',
+        name: 'PHP',
         percent: 30
       },
       {
-        name: 'Polymer',
+        name: 'MySQL',
         percent: 10
       }
     ],
-    title: 'Some great title'
+    title: 'SAM PPH'
   },
   {
     area: 'p2',
-    date: new Date(),
+    date: new Date("09/28/2016"),
     id: 2,
-    image: 'https://unsplash.it/1920/1081?random',
+    image: 'http://maciejbakowicz.pl/img/galeria/toms.png',
     link: '#',
     technologies: [
       {
-        name: 'Angular',
-        percent: 60
+        name: 'Photoshop',
+        percent: 90
       },
       {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
+        name: 'User experience',
         percent: 10
       }
     ],
-    title: 'Some great title'
+    title: 'Tom\'s Sneakers'
   },
   {
     area: 'p3',
-    date: new Date('07/30/1995'),
+    date: new Date("08/23/2016"),
     id: 3,
-    image: 'https://unsplash.it/1602/900?random',
+    image: 'http://maciejbakowicz.pl/img/galeria/witcher.png',
     link: '#',
     technologies: [
       {
-        name: 'Angular',
-        percent: 60
+        name: 'Photoshop',
+        percent: 90
       },
       {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
+        name: 'User experience',
         percent: 10
       }
     ],
-    title: 'Some great title'
+    title: 'Game development'
   },
   {
     area: 'p4',
-    date: new Date(),
-    id: 4,
-    image: 'https://unsplash.it/1920/1082?random',
+    date: new Date("08/23/2016"),
+    id: 3,
+    image: 'http://maciejbakowicz.pl/img/galeria/witcher.png',
     link: '#',
     technologies: [
       {
-        name: 'Angular',
+        name: 'AngularJS',
         percent: 60
       },
       {
-        name: 'Django',
+        name: 'PHP',
         percent: 30
       },
       {
-        name: 'Polymer',
+        name: 'MySQL',
         percent: 10
       }
     ],
-    title: 'Some great title'
-  },
-  {
-    area: 'p5',
-    date: new Date(),
-    id: 5,
-    image: 'https://unsplash.it/1921/1082?random',
-    link: '#',
-    technologies: [
-      {
-        name: 'Angular',
-        percent: 60
-      },
-      {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
-        percent: 10
-      }
-    ],
-    title: 'Some great title'
-  },
-  {
-    area: 'p6',
-    date: new Date(),
-    id: 6,
-    image: 'https://unsplash.it/1924/1082?random',
-    link: '#',
-    technologies: [
-      {
-        name: 'Angular',
-        percent: 60
-      },
-      {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
-        percent: 10
-      }
-    ],
-    title: 'Some great title'
-  },
-  {
-    area: 'p7',
-    date: new Date(),
-    id: 7,
-    image: 'https://unsplash.it/1922/1080?random',
-    link: '#',
-    technologies: [
-      {
-        name: 'Angular',
-        percent: 60
-      },
-      {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
-        percent: 10
-      }
-    ],
-    title: 'Some great title'
-  },
-  {
-    area: 'p8',
-    date: new Date(),
-    id: 8,
-    image: 'https://unsplash.it/1919/1081?random',
-    link: '#',
-    technologies: [
-      {
-        name: 'Angular',
-        percent: 60
-      },
-      {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
-        percent: 10
-      }
-    ],
-    title: 'Some great title'
-  },
-  {
-    area: 'p9',
-    date: new Date(),
-    id: 9,
-    image: 'https://unsplash.it/1920/1088?random',
-    link: '#',
-    technologies: [
-      {
-        name: 'Angular',
-        percent: 60
-      },
-      {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
-        percent: 10
-      }
-    ],
-    title: 'Some great title'
-  },
-  {
-    area: 'p10',
-    date: new Date(),
-    id: 10,
-    image: 'https://unsplash.it/1922/1084?random',
-    link: '#',
-    technologies: [
-      {
-        name: 'Angular',
-        percent: 60
-      },
-      {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
-        percent: 10
-      }
-    ],
-    title: 'Some great title'
-  },
-  {
-    area: 'p11',
-    date: new Date(),
-    id: 11,
-    image: 'https://unsplash.it/1919/1088?random',
-    link: '#',
-    technologies: [
-      {
-        name: 'Angular',
-        percent: 60
-      },
-      {
-        name: 'Django',
-        percent: 30
-      },
-      {
-        name: 'Polymer',
-        percent: 10
-      }
-    ],
-    title: 'Some great title'
+    title: 'Band website'
   }
 ];
